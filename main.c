2f95f695e0ff028bd3c4ab54941ae14003db4c99
dunisan <57772699+dunisan@users.noreply.github.com>
2022-11-20 13:32:21 +0200

    Update main.c
    4'th change

--- main.c
+++ main.c
@@ -5,6 +5,20 @@ int main(){
     int i;
     printf("please enter two positive numbers: \n"); 
     scanf("%d %d", &num1, &num2);
+    
+    printf("The Armstorng numbers are: ");
+    for(i=num1;i<=num2;i++){
+        if(isArmstrong(i))
+            printf("%d ", i);
+    }
+    printf("\n");
+    
+    printf("The Palindrome numbers are: ");
+    for(i=num1;i<=num2;i++){
+        if(isPalindrome(i))
+            printf("%d ", i);
+    }
+    printf("\n");
 
     printf("The prime numbers are: ");
     for(i=num1;i<=num2;i++){
@@ -13,29 +27,11 @@ int main(){
     }
     printf("\n");
     
-    printf("The Armstorng numbers are: ");
-    for(i=num1;i<=num2;i++){
-        if(isArmstrong(i))
-            printf("%d ", i);
-    }
-    printf("\n");
-
     printf("The Strong numbers are: ");
     for(i=num1;i<=num2;i++){
         if(isStrong(i))
             printf("%d ", i);
     }
-    printf("\n");
-
-    printf("The Palindrome numbers are: ");
-    for(i=num1;i<=num2;i++){
-        if(isPalindrome(i))
-            printf("%d ", i);
-    }
-
-    
-    printf("\n");
-
     
     
     return 0;
