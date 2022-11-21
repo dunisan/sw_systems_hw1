d1774d53807a22defbb12527f146326390bd49bc
dunisan <57772699+dunisan@users.noreply.github.com>
2022-11-21 15:47:19 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -15,7 +15,7 @@ int isPalindrome();
 /* will return if a number is prime */
 int isPrime();
 
-/*Strong number is a special number whose sum of the factorial of digits is equal to the original number. For Example: 145 is strong number*/
+/*Strong number is a special number whose sum of the factorial of digits is equal to the original number. For Example: 145 is strong number 
 145= !5+ !4+ !1 I*/
 int isStrong();
 
